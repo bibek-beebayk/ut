a67813862bebbffd8dbcc94e0c6fe6ff1bc07e50
Bibek Gautam <beebayk0001@gmail.com>
2024-04-14 16:17:58 +0545

    chore: settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -9,6 +9,18 @@ ALLOWED_HOSTS = ["ut-production.up.railway.app"]
 
 CSRF_TRUSTED_ORIGINS = ["https://ut-production.up.railway.app"]
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = "1f8bcff6b47d61799db7beaae02a3fa4"
+AWS_SECRET_ACCESS_KEY = (
+    "78e32042ccce9896aa5b5767338949146eda472fd532328302be87d2b0ed5260"
+)
+AWS_STORAGE_BUCKET_NAME = "bonewa"
+AWS_S3_ENDPOINT_URL = (
+    "https://b46a64eb384beb50a5fc80946bc0abc7.r2.cloudflarestorage.com/bonewa"
+)
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
